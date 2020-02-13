2943811d9d283a9cb42c50e9f3cc739db17ecc3f
frknckc <furkancakici25@gmail.com>
2020-02-13 20:36:53 +0300

    Up:3

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 function Navbar() {
     return (
         <div >
-            <nav className="container-fluid navbar navbar-expand-lg navbar-light bg-light h5" >
+            <nav className="container-fluid navbar navbar-expand-lg navbar-light bg-light h4" >
                 <NavLink className="navbar-brand navbar-expand-md" to="/home" ><i className="fas fa-globe-asia fa-spin display-4" style={{ color: "#1B9CFC" }}></i></NavLink>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
